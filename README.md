8b0d50c5f6210bb40a45178e3c97d84b9e18ed0d
neo-jgrec <jean-yanis.jeffroy@epitech.eu>
2024-03-05 19:29:57 +0100

    feat(project): front showcase web page

---
+++ README.md
@@ -0,0 +1,7 @@
+# outer.chat showcase
+
+This repository contains the code for the chat showcase of the [outer.chat](https://github.com/outer-char) project.
+
+> [!WARNING]
+> The page is under construction.  
+> It's here to make a use of the `outer.chat` domain.
---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import logo from './assets/outer.chat.svg'
+
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-900">
+      <img src={logo} alt="Logo" className="object-center object-cover w-1/3" />
+    </div>
+  )
+}
+
+export default App
